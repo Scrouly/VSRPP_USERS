39737093ab95dab3f28436ed7c5ff419350aca6e
Scrouly <fevanwood@yahoo.com>
2022-11-29 14:31:56 +0300

    Change 404

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,22 +1,59 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
+import { Box, Button, Typography } from '@mui/material'
 const NotFound = () => {
+  //   return (
+  //     <div class="mainbox">
+  //       <div class="err">404</div>
+  //       <div class="msg">
+  //         Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
+  //         existed in the first place?
+  //         <p>
+  //           Let's go{' '}
+  //           <NavLink to="." end>
+  //             Home
+  //           </NavLink>{' '}
+  //           and try from there.
+  //         </p>
+  //       </div>
+  //     </div>
+  //   )
+  // }
+  const navigate = useNavigate()
+
   return (
-    <div class="mainbox">
-      <div class="err">404</div>
-      <div class="msg">
-        Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
-        existed in the first place?
-        <p>
-          Let's go{' '}
-          <NavLink to="." end>
-            Home
-          </NavLink>{' '}
-          and try from there.
-        </p>
-      </div>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        minHeight: '60vh',
+        backgroundColor: '#494949',
+      }}
+    >
+      <Typography variant="h1" style={{ color: 'white' }}>
+        404
+      </Typography>
+      <Typography variant="h6" style={{ color: 'white' }}>
+        The page you’re looking for doesn’t exist.
+      </Typography>
+      <Typography variant="h6" style={{ color: 'white' }}>
+        Maybe this page moved? Got deleted? Is hiding out in quarantine?
+      </Typography>
+      <Typography variant="h6" style={{ color: 'white' }}>
+        Never existed in the first place?Let's go Home and try from there.
+      </Typography>
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate('/')
+        }}
+      >
+        Back Home
+      </Button>
+    </Box>
   )
 }
 
